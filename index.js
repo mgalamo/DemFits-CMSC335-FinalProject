a59b306309540ad5df05b54075f293ce68293d65
oluwadamilola Ogunbode <dammyog@terpmail.com>
2025-12-12 23:46:25 -0500

    placeorder blurb

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
   res.send("index.html");
 });
 
+app.get("/placeorder", (req, res) => {
+  res.render("placeorder");
+});
+
+app.get("/getinspirations", (req, res) => {
+  res.render("getinspirations");
+});
+
+app.get("/reviewdates", (req, res) => {
+  res.render("reviewdates");
+});
+
+app.get("/cancelorder", (req, res) => {
+  res.render("cancelorder");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
